1fc605e6defa582cc78ef142bcd5973a1c5eb710
Rajni Kant Roy <38808588+rajnikantroy@users.noreply.github.com>
2018-10-08 17:21:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get('/index', function(req, res) {
 });
 app.get('/', function(req, res) {
 	console.log('from root');
+	console.log('query'+req.query.instance_url);
 	logQueryParam(req, res);
     res.sendFile(path.join(__dirname + '/View/index.html'));	
 });
