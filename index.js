b13b5e1d4856fc9cb5936739229d70f4ea3b4b7f
Rajni Kant Roy <38808588+rajnikantroy@users.noreply.github.com>
2018-10-08 16:18:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ var path = require('path');
 // viewed at http://localhost:8080
 app.get('/index', function(req, res) {
     res.sendFile(path.join(__dirname + '/View/index.html'));
+	logQueryParam(req, res);
 });
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/View/index.html'));
+	logQueryParam(req, res);
 });
+
+function logQueryParam(req, res){
+	if(req.query.instance_url && req.query.access_token){
+		console.log('req.query.instance_url : '+ req.query.instance_url);
+		console.log('req.query.access_token : '+req.query.access_token);
+	}
+}
 
 //app.listen(8080);
 app.listen(process.env.port || process.env.PORT || 8080);
