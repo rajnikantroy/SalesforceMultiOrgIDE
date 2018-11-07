955c05d49276d7a684ed292c09f1cfb62debc908
Rajni Kant Roy <38808588+rajnikantroy@users.noreply.github.com>
2018-11-07 17:07:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,29 +3,54 @@ var jsforce = require('jsforce');
 var app = express();
 var path = require('path');
 var sf = require('node-salesforce');
+var environments = [];
 
 // viewed at http://localhost:8080
 app.get('/index', function(req, res) {
 	console.log('from index');
-	//logQueryParam(req, res);
+	//salesforceParams(req, res);
     res.sendFile(path.join(__dirname + '/View/index.html'));
 });
 
 app.get('/', function(req, res) {
 	console.log('request: '+req.param('abc'));
 	//console.log('req.query.instance_url - '+req.query.instance_url);
-	logQueryParam(req, res);
+	salesforceParams(req, res);
     res.sendFile(path.join(__dirname + '/View/index.html'));	
 });
 
-function logQueryParam(req, res){
+function salesforceParams(req, res){
 	if(req.query.instance_url && req.query.access_token){
 		console.log('req.query.instance_url : '+ req.query.instance_url);
 		console.log('req.query.access_token : '+req.query.access_token);
 		console.log('req.query.token_type : '+req.query.token_type);
-		loginSalesforce(req.query.instance_url, req.query.access_token);
+		var environment = {};
+		environment.url = req.query.instance_url;
+		environment.token = req.query.access_token;
+		environment.token_type = req.query.token_type;
+		environment.scope = req.query.scope;
+		environments.push(environment);
+		//QueryAccount(req.query.instance_url, req.query.access_token);
+		QueryAccount();
 	}
 }
+function QueryAccount(){
+	var conn = new sf.Connection({
+	  instanceUrl : environments[0].url,
+	  accessToken : environments[0].token
+	});
+	console.log('environments.length : '+environments.length);
+	var records = [];
+	conn.query("SELECT Id, Name FROM ApexClass limit 10", function(err, result) {
+	  if (err) { return console.error(err); }
+	  console.log("total : " + result.totalSize);
+	  console.log("fetched : " + result.records.length);
+	  for(var i=0; i<result.records.length; i++){
+			console.log(result.records[i].Name);
+	  }
+	});
+}
+
 function loginSalesforce(url, token){
 	var conn = new sf.Connection({
 	  instanceUrl : url,
@@ -36,7 +61,11 @@ function loginSalesforce(url, token){
 	  if (err) { return console.error(err); }
 	  console.log("total : " + result.totalSize);
 	  console.log("fetched : " + result.records.length);
+	  for(var i=0; i<result.records.length; i++){
+			console.log(result.records[0].Name);
+	  }
 	});
+	
 }
 //app.listen(8080);
 app.listen(process.env.port || process.env.PORT || 8080);
