6a35172c86441319e2b0a244a3822608687ef1b2
Rajni Kant Roy <38808588+rajnikantroy@users.noreply.github.com>
2018-11-09 02:29:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var metadata = require('./nodejs/MetaDataContainer.js');
 var express = require('express');
 var jsforce = require('jsforce');
 var app = express();
@@ -5,21 +6,26 @@ var path = require('path');
 var sf = require('node-salesforce');
 var environments = [];
 
-// viewed at http://localhost:8080
 app.get('/index', function(req, res) {
 	console.log('from index');
-	//salesforceParams(req, res);
+	getQueryParamsFromMainPage(req, res);
     res.sendFile(path.join(__dirname + '/View/index.html'));
 });
 
+app.get('/list', function(req, res) {
+	console.log('from list-compare');
+	//CollectQueryParams(req, res);
+    res.sendFile(path.join(__dirname + '/View/list-compare.html'));  
+});
+
 app.get('/', function(req, res) {
-	console.log('request: '+req.param('abc'));
+	//console.log('request: '+req.param('abc'));
 	//console.log('req.query.instance_url - '+req.query.instance_url);
-	salesforceParams(req, res);
+	getQueryParamsFromMainPage(req, res);
     res.sendFile(path.join(__dirname + '/View/index.html'));	
 });
 
-function salesforceParams(req, res){
+function getQueryParamsFromMainPage(req, res){
 	if(req.query.instance_url && req.query.access_token){
 		console.log('req.query.instance_url : '+ req.query.instance_url);
 		console.log('req.query.access_token : '+req.query.access_token);
@@ -30,18 +36,50 @@ function salesforceParams(req, res){
 		environment.token_type = req.query.token_type;
 		environment.scope = req.query.scope;
 		environments.push(environment);
-		//QueryAccount(req.query.instance_url, req.query.access_token);
-		QueryAccount();
+		QueryAccount(req, res);
+		/*storeEnvironmentDetails();
+		navigateToWelcomePage();
+		var metadata = getParamsFromWelcomePage(req, res);
+		*/
 	}
 }
-function QueryAccount(){
+function getConnection(){
+	var latestEnvironment = environments.length;
 	var conn = new sf.Connection({
-	  instanceUrl : environments[0].url,
-	  accessToken : environments[0].token
+	  instanceUrl : environments[latestEnvironment-1].url,
+	  accessToken : environments[latestEnvironment-1].token
 	});
+	return conn;
+}
+function navigateToWelcomePage(){
+	res.sendFile(path.join(__dirname + '/View/list-compare.html'));
+}
+
+function getParamsFromWelcomePage(req, res){
+	var mtdata = req.param('selectedMetadata');
+	return mtdata;
+	
+}
+
+function queryMetadata(meta){
+	var conn = getConnection();
+	return metadata.getMetadata(meta);
+}
+
+function QueryAccount(req, res){
+	var latestEnvironment = environments.length;
+	var conn = new sf.Connection({
+	  instanceUrl : environments[latestEnvironment-1].url,
+	  accessToken : environments[latestEnvironment-1].token
+	});
+	
 	console.log('environments.length : '+environments.length);
 	var records = [];
+	//console.log('conn: '+JSON.stringify(conn));
+	console.log("conn status: "+conn.status);
+	res.sendFile(path.join(__dirname + '/View/list-compare.html'));  
 	conn.query("SELECT Id, Name FROM ApexClass limit 10", function(err, result) {
+		
 	  if (err) { return console.error(err); }
 	  console.log("total : " + result.totalSize);
 	  console.log("fetched : " + result.records.length);
@@ -65,7 +103,5 @@ function loginSalesforce(url, token){
 			console.log(result.records[0].Name);
 	  }
 	});
-	
 }
-//app.listen(8080);
 app.listen(process.env.port || process.env.PORT || 8080);
